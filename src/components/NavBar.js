07cd1a32fe1c3660f5b543133a93122bede6b83e
Avadhkumar-geek <avadhkachhadiya@gmail.com>
2022-11-19 11:32:19 +0530

    changed margin of navbar contents

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,35 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function NavBar(props) {
-
     return (
         <>
             <div>
                 <nav className="navbar">
                     <div>
-                        <Link className="navbar-brand ms-5" to='/'>
-                            <img src={require('./logo192.png')} alt="" width="30" height="30" />
+                        <Link className="navbar-brand ms-3" to="/">
+                            <img src={require("./logo192.png")} alt="" width="30" height="30" />
                         </Link>
                     </div>
-                    <ul className='link'>
+                    <ul className="link">
                         <li>
-                            <Link className='link' to={'/'} >Home</Link>
+                            <Link className="link" to={"/"}>
+                                Home
+                            </Link>
                         </li>
                         <li>
-                            <Link className='link' to={'/'} >About</Link>
+                            <Link className="link" to={"/"}>
+                                About
+                            </Link>
                         </li>
                         <li>
-                            <div className="form-check form-switch mt-1 me-5">
-                                <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch"
-                                    id="flexSwitchCheckDefault" />
-                                <label className="form-check-label" style={props.mode} htmlFor="flexSwitchCheckDefault">Dark Mode</label>
+                            <div className="form-check form-switch mt-1 me-3">
+                                <input
+                                    className="form-check-input"
+                                    onClick={props.toggleMode}
+                                    type="checkbox"
+                                    role="switch"
+                                    id="flexSwitchCheckDefault"
+                                />
+                                <label
+                                    className="form-check-label"
+                                    style={props.mode}
+                                    htmlFor="flexSwitchCheckDefault"
+                                >
+                                    Dark Mode
+                                </label>
                             </div>
                         </li>
                     </ul>
                 </nav>
             </div>
-
         </>
     );
 }
